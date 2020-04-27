751384af79233b45115c9ad680ff106afff19dfb
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2020-04-26 22:41:26 -0400

    create standard button

--- src/lib/ds/button.stories.tsx
+++ src/lib/ds/button.stories.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import Button from "./button";
+import Button, { Variant, Color } from "./button";
 
 export default {
     title: "DS|Button",
     component: Button
 }
 
-export const nakedButton = () => (
-    <Button>My Button</Button>
+export const basicNakedButton = () => (
+    <Button variant={Variant.Naked}>Basic Button</Button>
 );
+
+export const dangerNakedButton = () => (
+    <Button variant={Variant.Naked} color={Color.Danger}>Danger Button</Button>
+);
+
+export const basicStandardButton = () => (
+    <Button variant={Variant.Standard}>Standard Button</Button>
+);
+
+export const dangerStandardButton = () => (
+    <Button variant={Variant.Standard} color={Color.Danger}>Danger Button</Button>
+);
+
+export const withEndIcon = () => (
+    <Button variant={Variant.Standard} color={Color.Danger} endIcon="▼">Danger Button</Button>
+)
---
+++ src/lib/ds/button.tsx
@@ -0,0 +1,127 @@
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import tokens from "./tokens";
+
+export enum Variant {
+    Standard = "standard",
+    Naked = "naked",
+}
+
+export enum Color {
+    Danger = "danger",
+    Default = "default"
+}
+
+interface ButtonProps {
+    variant?: Variant;
+    color?: Color;
+    startIcon?: any;
+    endIcon?: any;
+    children: any;
+}
+
+// this is a private implementation that should not be exported
+const BaseButton = styled.button<ButtonProps>`
+    font-family: ${tokens.fontFamily};
+    font-size: ${tokens.typeScale[2]}px;
+    border-width: 1px;
+    border-style: solid;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+
+const getNakedButtonFontColor = (props: ButtonProps) => {
+    switch (props.color) {
+        case Color.Danger:
+            return tokens.colors.red;
+        default:
+            return tokens.colors.gray800;
+        
+    }
+}
+
+const NakedButton = styled(BaseButton)`
+    color: ${getNakedButtonFontColor};
+    border-color: transparent;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const getStandardButtonBackgroundColor = (props: ButtonProps) => {
+    switch (props.color) {
+        case Color.Danger:
+            return tokens.colors.red;
+        default:
+            return tokens.colors.white;
+    }
+};
+
+const getStandardButtonBorderColor = (props: ButtonProps) => {
+    switch (props.color) {
+        case Color.Danger:
+            return tokens.colors.red;
+        default:
+            return tokens.colors.gray300;
+    }
+}
+
+const getStandardButtonFontColor = (props: ButtonProps) => {
+    if(props.color === Color.Danger) {
+        return tokens.colors.white;
+    } else {
+        return tokens.colors.gray800;
+    }
+}
+
+const StandardButton = styled(BaseButton)`
+    border-radius: 5px;
+    background-color: ${getStandardButtonBackgroundColor};
+    border-color: ${getStandardButtonBorderColor};
+    color: ${getStandardButtonFontColor};
+    height: ${tokens.spaces[3]}px;
+`;
+
+const getButtonComponentByVariant = (variant: Variant) => {
+    switch (variant) {
+        case Variant.Naked:
+            return NakedButton;
+        default:
+            return StandardButton;
+    }
+}
+
+const ButtonStartIconContainer = styled.span`
+    margin-right: ${tokens.spaces[1]}px;
+`;
+
+const ButtonEndIconContainer = styled.span`
+    margin-left: ${tokens.spaces[1]}px;
+`;
+
+const Button = (props: ButtonProps) => {
+    const ButtonVariant = useMemo(
+        () => getButtonComponentByVariant(props.variant!),
+        [props.variant]
+    );
+
+    const {children, startIcon, endIcon, ...rest} = props;
+
+    return (
+        <ButtonVariant {...rest}>
+            {startIcon && <ButtonStartIconContainer>{startIcon}</ButtonStartIconContainer>}
+            {children}
+            {endIcon && <ButtonEndIconContainer>{endIcon}</ButtonEndIconContainer>}
+
+        </ButtonVariant>
+    );
+}
+
+Button.defaultProps = {
+    variant: Variant.Standard,
+    color: Color.Default
+}
+
+export default Button;
